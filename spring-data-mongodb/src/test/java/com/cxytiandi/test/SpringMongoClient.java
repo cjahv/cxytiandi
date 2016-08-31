da3007062f594db352c8c912da137f3f9d8bde56
yinjihuan <jihuan900@126.com>
2016-08-31 22:10:41 +0800

    commit update code

--- spring-data-mongodb/src/test/java/com/cxytiandi/test/SpringMongoClient.java
+++ spring-data-mongodb/src/test/java/com/cxytiandi/test/SpringMongoClient.java
@@ -8,6 +8,10 @@ import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.core.query.Update.Position;
 
 import com.cxytiandi.mongo.document.Article;
 /**
@@ -25,7 +29,49 @@ public class SpringMongoClient {
 	}
 	
 	public static void main(String[] args)  throws Exception {
-		initArticle();
+		//initArticle();
+		updateArticle();
+	}
+	
+	public static void updateArticle() {
+		//修改第一条author为yinjihuan的数据中的title和visitCount
+		Query query = Query.query(Criteria.where("author").is("yinjihuan"));
+		Update update = Update.update("title", "MongoTemplate").set("visitCount", 10);
+		mongoTemplate.updateFirst(query, update, Article.class);
+		
+		//修改全部符合条件的
+		mongoTemplate.updateMulti(query, update, Article.class);
+		
+		//特殊更新，更新author为jason的数据，如果没有author为jason的数据则以此条件创建一条新的数据
+		//当没有符合条件的文档，就以这个条件和更新文档为基础创建一个新的文档，如果找到匹配的文档就正常的更新。
+		query = Query.query(Criteria.where("author").is("jason"));
+		update = Update.update("title", "MongoTemplate").set("visitCount", 10);
+		mongoTemplate.upsert(query, update, Article.class);
+		
+		//更新条件不变，更新字段改成了一个我们集合中不存在的，用set方法如果更新的key不存在则创建一个新的key
+		query = Query.query(Criteria.where("author").is("jason"));
+		update = Update.update("title", "MongoTemplate").set("money", 100);
+		mongoTemplate.updateMulti(query, update, Article.class);
+		
+		//update的inc方法用于做累加操作，将money在之前的基础上加上100
+		query = Query.query(Criteria.where("author").is("jason"));
+		update = Update.update("title", "MongoTemplate").inc("money", 100);
+		mongoTemplate.updateMulti(query, update, Article.class);
+		
+		//update的rename方法用于修改key的名称
+		query = Query.query(Criteria.where("author").is("jason"));
+		update = Update.update("title", "MongoTemplate").rename("visitCount", "vc");
+		mongoTemplate.updateMulti(query, update, Article.class);
+		
+		//update的unset方法用于删除key
+		query = Query.query(Criteria.where("author").is("jason"));
+		update = Update.update("title", "MongoTemplate").unset("vc");
+		mongoTemplate.updateMulti(query, update, Article.class);
+		
+		//update的pull方法用于删除tags数组中的java
+		query = Query.query(Criteria.where("author").is("jason"));
+		update = Update.update("title", "MongoTemplate").pull("tags", "java");
+		mongoTemplate.updateMulti(query, update, Article.class);
 	}
 	
 	/**
